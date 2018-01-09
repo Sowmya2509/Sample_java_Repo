de9d4fabaa124cc658c22a633a1600baab7acc87
Sowmya <sowmi25993@gmail.com>
2018-01-09 16:32:31 +0530

    comment

---
+++ Sample_java_examples/src/com/java/examples/Hello.java
@@ -0,0 +1,9 @@
+package com.java.examples;
+
+public class Hello {
+
+    public static void main(String args[]){
+        System.out.println("Hello S owmya");
+
+    }
+}
---
+++ Sample_java_examples/src/com/java/examples/Hello3.java
@@ -0,0 +1,8 @@
+package com.java.examples;
+
+public class Hello3 {
+    public static void main(String args[]){
+        System.out.println("Hello S owmya");
+
+    }
+}
